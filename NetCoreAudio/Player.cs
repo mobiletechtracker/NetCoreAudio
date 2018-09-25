7e96727a57963f215e31cda82477dedca94a2ffd
fiodarsazanavets <fiodar.sazanavets@blackbox-co.com>
2018-09-25 18:31:23 +0100

    Apply public class ducumentation

--- NetCoreAudio/Player.cs
+++ NetCoreAudio/Player.cs
@@ -10,10 +10,19 @@ namespace NetCoreAudio
     {
         private readonly IPlayer _internalPlayer;
 
-        public event EventHandler PlaybackFinished;
+		/// <summary>
+		/// Internally, sets Playing flag to false. Additional handlers can be attached to it to handle any custom logic.
+		/// </summary>
+		public event EventHandler PlaybackFinished;
 
-        public bool Playing => _internalPlayer.Playing;
+		/// <summary>
+		/// Indicates that the audio is currently playing.
+		/// </summary>
+		public bool Playing => _internalPlayer.Playing;
 
+		/// <summary>
+		/// Indicates that the audio playback is currently paused.
+		/// </summary>
 		public bool Paused => _internalPlayer.Paused;
 
 		public Player()
@@ -30,22 +39,39 @@ namespace NetCoreAudio
             _internalPlayer.PlaybackFinished += OnPlaybackFinished;
         }
 
-        public async Task Play(string fileName)
+		/// <summary>
+		/// Will stop any current playback and will start playing the specified audio file. The fileName parameter can be an absolute path or a path relative to the directory where the library is located. Sets Playing flag to true. Sets Paused flag to false.
+		/// </summary>
+		/// <param name="fileName"></param>
+		/// <returns></returns>
+		public async Task Play(string fileName)
         {
             await _internalPlayer.Play(fileName);
         }
 
-        public async Task Pause()
+		/// <summary>
+		/// Pauses any ongong playback. Sets Paused flag to true. Doesn't modify Playing flag.
+		/// </summary>
+		/// <returns></returns>
+		public async Task Pause()
         {
             await _internalPlayer.Pause();
         }
 
-        public async Task Resume()
+		/// <summary>
+		/// Resumes any paused playback. Sets Paused flag to false. Doesn't modify Playing flag.
+		/// </summary>
+		/// <returns></returns>
+		public async Task Resume()
         {
             await _internalPlayer.Resume();
         }
 
-        public async Task Stop()
+		/// <summary>
+		/// Stops any current playback and clears the buffer. Sets Playing and Paused flags to false.
+		/// </summary>
+		/// <returns></returns>
+		public async Task Stop()
         {
             await _internalPlayer.Stop();
         }
