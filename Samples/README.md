d5ca7d5cbb6d7843c6bb892c21d775f780d0d1c0
empowerFiodar <fiodar@empower.me>
2024-02-18 19:48:42 +0000

    Add a code sample for UNO MVVM

---
+++ Samples/README.md
@@ -0,0 +1,7 @@
+# NetCoreAudio samples
+
+This folder contains various code samples of using NetCoreAudio inside different application types. It contains the following projects:
+
+## UnoMvvmAudioPlayer
+
+This project shows how to use NetCoreAudio inside an [UNO MVVM](https://unoplatformdocstaging.z13.web.core.windows.net/articles/external/workshops/simple-calc/modules/MVVM-XAML/04-App%20Architecture/README.html) applictaion.
---
+++ Samples/UnoMvvmAudioPlayer/AudioPlayer/MainModel.cs
@@ -0,0 +1,64 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using NetCoreAudio;
+
+namespace AudioPlayer;
+
+internal partial class MainModel : ObservableObject
+{
+    private readonly Player _player;
+
+    public MainModel()
+    {
+        _player = new();
+        //_player.PlaybackFinished += OnPlaybackFinished;
+    }
+
+    [ObservableProperty]
+    private string _fileName = string.Empty;
+
+    [ObservableProperty]
+    private bool _playing = false;
+
+    [ObservableProperty]
+    private bool _paused = false;
+
+    [RelayCommand]
+    private async Task Play()
+    {
+        await _player.Play(FileName);
+        UpdateStatuses();
+    }
+
+    [RelayCommand]
+    private async Task Pause()
+    {
+        await _player.Pause();
+        UpdateStatuses();
+    }
+
+    [RelayCommand]
+    private async Task Resume()
+    {
+        await _player.Resume();
+        UpdateStatuses();
+    }
+
+    [RelayCommand]
+    private async Task Stop()
+    {
+        await _player.Stop();
+        UpdateStatuses();
+    }
+
+    private void OnPlaybackFinished(object? sender, EventArgs? e)
+    {
+        UpdateStatuses();
+    }
+
+    private void UpdateStatuses()
+    {
+        Playing = _player.Playing;
+        Paused = _player.Paused;
+    }
+}
